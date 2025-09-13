5ebffb2226657b83c7dfd93f924d1fb4f2110ac1
KundanBhagat0204 <kundanbhagat7777@gmail.com>
2025-09-13 11:38:23 +0530

    Creating SignUp Page

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent {
+  Signup(item: NgForm) {
+    if (item.valid) {
+      console.log('login data', item);
+    } else {
+      console.log('invalid form!');
+    }
+  }
 
+  year = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+  ];
 }
