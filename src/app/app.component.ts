ee5da15027848eaf9e73d25fce168d71258f470a
KundanBhagat0204 <kundanbhagat7777@gmail.com>
2025-09-14 18:50:36 +0530

    Adding Dark Mode Functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Facebook';
+  isDark = false;
+  ngOnInit() {
+    const theme = localStorage.getItem('theme');
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    }
+  }
+
+  toggleDarkMode() {
+    const html = document.documentElement;
+    if (html.classList.contains('dark')) {
+      html.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+      this.isDark = false;
+    } else {
+      html.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+      this.isDark = true;
+    }
+  }
 }
