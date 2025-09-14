fb4905984347a06770e369927594a9e59b0dde26
KundanBhagat0204 <kundanbhagat7777@gmail.com>
2025-09-14 18:51:32 +0530

    Adding Dark Styling to Layout

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -15,8 +15,31 @@ export class SignupComponent {
     }
   }
 
-  year = [
+  date = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
     22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
   ];
+
+  month = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'July',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  years: number[] = [];
+
+  ngOnInit() {
+    for (let year = 2025; year >= 1905; year--) {
+      this.years.push(year);
+    }
+  }
 }
