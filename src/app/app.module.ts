8a0efc9ce165e667e279329558b18dc49b18ded4
KundanBhagat0204 <kundanbhagat7777@gmail.com>
2025-09-20 18:54:39 +0530

    Adding New Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,10 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { ReelsComponent } from './reels/reels.component';
 @NgModule({
-  declarations: [AppComponent, LoginComponent, SignupComponent],
+  declarations: [AppComponent, LoginComponent, SignupComponent, HomeComponent, ReelsComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
